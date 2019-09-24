d20ecad9a129c0f937fcade6502d7b486d5e2da2
immaniv <immaniv@outlook.com>
2019-09-24 14:05:12 -0700

    no red ball for fantasy5

--- lt_math.c
+++ lt_math.c
@@ -6,10 +6,17 @@
 
 int lt_factorial(uint total_num, uint draw_num, uint ad_num)
 {
-  for (int i = 1; i <= draw_num; i++) {
-    printf("% 20ld - ", (long) (FACT(total_num)/(FACT(i)*FACT(total_num-i))));
-    printf("% 20ld\n", (long) ((FACT(total_num)/(FACT(i)*FACT(total_num-i)))*ad_num));
-  }
+  for (int i = 1; i <= draw_num; i++)
+    if (ad_num > 1) 
+      printf("(%d of %d) %10ld | (%d of %d + 1 of %d) %10ld\n",\
+           i, total_num,\
+          (long) (FACT(total_num)/(FACT(i)*FACT(total_num-i))),\
+           i, total_num, ad_num,\
+          (long) ((FACT(total_num)/(FACT(i)*FACT(total_num-i)))*ad_num));
+    else
+      printf("(%d of %d) %10ld\n",\
+           i, total_num,\
+          (long) (FACT(total_num)/(FACT(i)*FACT(total_num-i))));
 		  
   return 0;
 }
