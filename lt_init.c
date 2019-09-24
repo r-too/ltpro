481e4daae251b45630b1db22877dbea74fc30484
immaniv <immaniv@outlook.com>
2019-09-24 14:29:24 -0700

    chang memset location

--- lt_init.c
+++ lt_init.c
@@ -8,7 +8,6 @@ extern char *custom_opts;
 void lt_init(struct lt_query *q)
 {
 
-  memset(q->query_str, '\0', sizeof(q->query_str));
 
   switch (q->lt_type) {
   case MEGA_MILLIONS:
@@ -50,6 +49,7 @@ void lt_megamillions_init(struct lt_query *q)
   q->t_rnum = MM_RNUM;
   q->rn_rep = MM_RREP;
 
+  memset(q->query_str, '\0', strlen(MM_QSTR)+1);
   memcpy(q->query_str, MM_QSTR, strlen(MM_QSTR));
 }
 
@@ -67,6 +67,7 @@ void lt_powerball_init(struct lt_query *q)
   q->t_rnum = PB_RNUM;
   q->rn_rep = PB_RREP;
 
+  memset(q->query_str, '\0', strlen(PB_QSTR)+1);
   memcpy(q->query_str, PB_QSTR, strlen(PB_QSTR));
 }
 
@@ -83,7 +84,8 @@ void lt_superlotto_init(struct lt_query *q)
   q->rn_max = SL_RMAX;
   q->t_rnum = SL_RNUM;
   q->rn_rep = SL_RREP;
-
+  
+  memset(q->query_str, '\0', strlen(SL_QSTR)+1);
   memcpy(q->query_str, SL_QSTR, strlen(SL_QSTR));
 }
 
@@ -96,6 +98,7 @@ void lt_fantasy5_init(struct lt_query *q)
   q->t_wnum = F5_WNUM;
   q->wn_rep = F5_WREP;
   
+  memset(q->query_str, '\0', strlen(F5_QSTR)+1);
   memcpy(q->query_str, F5_QSTR, strlen(F5_QSTR));
 }
 
@@ -108,6 +111,7 @@ void lt_daily4_init(struct lt_query *q)
   q->t_wnum = D4_WNUM;
   q->wn_rep = D4_WREP;
  
+  memset(q->query_str, '\0', strlen(D4_QSTR)+1);
   memcpy(q->query_str, D4_QSTR, strlen(D4_QSTR));
 }
 
@@ -120,6 +124,7 @@ void lt_daily3_init(struct lt_query *q)
   q->t_wnum = D3_WNUM;
   q->wn_rep = D3_WREP;
 
+  memset(q->query_str, '\0', strlen(D3_QSTR)+1);
   memcpy(q->query_str, D3_QSTR, strlen(D3_QSTR));
 }
 
